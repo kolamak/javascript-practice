0d3d486dbf8f999c473ce8f6903ecc30449fe704
Makanjuola kolawole opemipo <kolamakanjuola@gmail.com>
2025-07-30 03:35:23 +0100

    practices

---
+++ index.js
@@ -0,0 +1,44 @@
+// 1. string variables
+// This code defines and logs string variables to the console.
+let name = 'Kolawole';
+console.log(name);
+
+// 2. number variables
+// This code defines and logs number variables to the console.
+let age = 30;
+console.log(age);
+
+// 3. boolean variables
+// This code defines and logs boolean variables to the console.
+let isStudent = true;
+console.log(isStudent);
+
+// 4. undefined variable
+// This code defines an undefined variable and logs it to the console.
+let address
+console.log(address);
+
+// 5. null variable
+// This code defines a null variable and logs it to the console.
+let phoneNumber = null;
+console.log(phoneNumber);
+
+// 6. symbol variable
+// This code defines a symbol variable and logs it to the console.
+
+// 7. bitgint variable
+// this is a bigint variable, which is not used in this code snippet
+
+// variables
+// This code defines a variable and logs it to the console.
+
+// i. var
+var city = 'Lagos';
+console.log(city);
+
+// ii. let
+let country = 'Nigeria';
+console.log(country);
+
+// iii. const
+const continent = 'Africa';
